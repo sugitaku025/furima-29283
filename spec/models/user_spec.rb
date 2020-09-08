68504169a28042d263dbda744c964c35cdb2abde
sugitaku025 <snowrabbit026@gmail.com>
2020-09-08 17:38:53 +0900

    レビュー訂正(2)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -49,6 +49,11 @@ describe User do
         another_user.valid?
         expect(another_user.errors.full_messages).to include("Email has already been taken")
       end
+      it "@の存在しないemailは登録できない" do
+        @user.email = "aaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
       it "passwordが空だと登録できない" do
         @user.password = ''
         @user.valid?
