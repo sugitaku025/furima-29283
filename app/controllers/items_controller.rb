8e466d2b26a6007f2aaabc4eeb141f28f72ba8d0
sugitaku025 <snowrabbit026@gmail.com>
2020-09-09 17:57:27 +0900

    非ログイン時の出品ページへのアクセス制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,12 @@
 class ItemsController < ApplicationController
+
   def index
     @items = Item.all.order("created_at DESC")
   end
 
   def new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
