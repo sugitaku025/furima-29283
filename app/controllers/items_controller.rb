83f401f95181a7b0698ee3aaa572e228fb946b68
sugitaku025 <snowrabbit026@gmail.com>
2020-09-09 19:27:46 +0900

    アソシエーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,4 +14,5 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  belongs_to :user
   has_one_attached :image
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :sender
+  belongs_to_active_hash :sending
 end
