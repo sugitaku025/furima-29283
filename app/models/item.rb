7a74e783e40c7a60e1a32b052d144265321e2f4a
sugitaku025 <snowrabbit026@gmail.com>
2020-09-10 14:33:49 +0900

    出品フォームのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :explanation
+    validates :category
+    validates :condition
+    validates :postage
+    validates :sender
+    validates :sending
+  end
+  PRICE_REGEX = /\A[\d]+\z/.freeze
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates_format_of :price, with: PRICE_REGEX
+
   belongs_to :user
   has_one_attached :image
   belongs_to_active_hash :category
