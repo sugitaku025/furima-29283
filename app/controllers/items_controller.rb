2b5b2d9dde52dcc7ee9a7189949626ff8fcd1093
sugitaku025 <snowrabbit026@gmail.com>
2020-09-11 17:00:44 +0900

    商品データのDB保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,11 +8,32 @@ class ItemsController < ApplicationController
     unless user_signed_in?
       redirect_to new_user_session_path
     end
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(
+      :image,
+      :name,
+      :price,
+      :explanation,
+      :category_id,
+      :condition_id,
+      :postage_id,
+      :sender_id,
+      :sending_id,
+    )
+    .merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -14,6 +14,14 @@ class Item < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates_format_of :price, with: PRICE_REGEX
 
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :condition_id
+    validates :postage_id
+    validates :sender_id
+    validates :sending_id
+  end
+
   belongs_to :user
   has_one_attached :image
   belongs_to_active_hash :category
