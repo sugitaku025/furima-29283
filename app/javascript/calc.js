8fd1362d28195e87abf97694d8fc7c1f22a77e89
sugitaku025 <snowrabbit026@gmail.com>
2020-09-14 12:18:46 +0900

    テストコードの入力

--- app/javascript/calc.js
+++ app/javascript/calc.js
@@ -7,9 +7,9 @@ window.addEventListener('load', function(){
 
   price.addEventListener('input', function() {
     const str = this.value
-    const fee = str * 0.1
+    const fee = Math.floor( str * 0.1 )
     profit = str - fee
-    commission.innerHTML = fee
+    commission.innerHTML = ( "Math.floor", fee)
     sales_profit.innerHTML = profit
   })
 })
