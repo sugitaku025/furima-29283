7907766cd0ea0c3100f006e7c1691704362f4262
sugitaku025 <snowrabbit026@gmail.com>
2020-09-16 14:41:01 +0900

    コードレビュー訂正(2)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,8 +27,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if user_signed_in? && current_user.id == @item.user_id
-      @item.destroy
+    if @item.destroy && current_user.id == @item.user_id
       redirect_to root_path
     else
       render :edit
