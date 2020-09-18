4a4988687da81d84a7ec097398f5e0bb59c5dfc9
sugitaku025 <snowrabbit026@gmail.com>
2020-09-18 17:49:41 +0900

    コントローラーの再設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,21 @@
 class OrdersController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:index]
+
   def index
     @item = Item.find(params[:item_id])
+    if current_user.id ==  @item.user_id
+      redirect_to items_path(@item)
+    end
     @order = Order.new
   end
 
   def create
   end
+end
 
+private
+
+@item = Item.find(params[:item_id])
+if current_user.id ==  @item.user_id
+  redirect_to items_path(@item)
 end
