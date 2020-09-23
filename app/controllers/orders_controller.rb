448bc9219a345f8f0c851ffd044a877a8a296ae8
sugitaku025 <snowrabbit026@gmail.com>
2020-09-23 09:48:44 +0900

    クレジット決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,16 +6,44 @@ class OrdersController < ApplicationController
     if current_user.id ==  @item.user_id
       redirect_to items_path(@item)
     end
-    @order = Order.new
+    @order = OrderAddress.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order = OrderAddress.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
-end
 
-private
+  private
 
-@item = Item.find(params[:item_id])
-if current_user.id ==  @item.user_id
-  redirect_to items_path(@item)
+  def order_params
+    params.permit(
+      :token,
+      :postal_code,
+      :prefecture_id,
+      :city,
+      :house_number,
+      :building,
+      :phone_number,
+      :item_id,
+    )
+    .merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency:'jpy'
+    )
+  end
 end
